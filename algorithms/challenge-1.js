dd361cdb01905bd9c9813a8028e64a26562c46c0
Tochukwu Jonah <jonahtochukwu@gmail.com>
2022-06-25 18:26:30 +0100

    Challenge 1 completed

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,23 @@
-function secondLargest(arr) {
+const numbers = [12, 2, 18, 93, 8, 51];
+
+const newNumber = numbers.sort( (a, b) => {
+    return b-a;
+})
+
+console.log(newNumber[1]);
+
+// function secondLargest(arr) {
   
-}
-module.exports = secondLargest
+// }
+// module.exports = secondLargest
+
+
+
+// # Challenge 1
+
+// Implement a function secondLargest(arr) that takes an array of numbers and returns the second largest number.
+
+// # Example
+//     secondLargest([6, 3, 0, 30, 7]) to return 7
+//     secondLargest([71, 33, 3, 51, 18, 6]) to return 51
+//     secondLargest([42, 68, 22, 60, 7]) to return 60
--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -2,4 +2,14 @@ function filterArray(arr) {
  
 }
 
-module.exports = filterArray
+// module.exports = filterArray
+
+
+// # Challenge 2
+
+// Implement a function filterArray(arr) that takes an array of non-negative integers and strings and return a new array without the strings.
+
+// # Example
+//     filterArray([1, 2, "a", "b"]) to return [1,2]
+//     filterArray([4, "z", "f", 5]) to return [4,5]
+//     filterArray(["$%^", 567, "&&&"]) to return [567]
